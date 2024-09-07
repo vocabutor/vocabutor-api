a5a99c378924ffd06a3f8df126cab584cabe9de0
Nima Ajdari <me@nimaajdari.com>
2024-09-07 23:40:29 +0200

    feat: google oauth impl

--- src/main/kotlin/com/vocabutor/Application.kt
+++ src/main/kotlin/com/vocabutor/Application.kt
@@ -1,12 +1,54 @@
 package com.vocabutor
 
 import com.vocabutor.plugins.*
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.server.application.*
+import io.ktor.server.sessions.*
+import io.ktor.client.engine.cio.*
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.serialization.kotlinx.json.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
-fun Application.module() {
-    configureRouting()
+val applicationHttpClient = HttpClient(CIO) {
+    install(ContentNegotiation) {
+        json()
+    }
+}
+
+fun Application.module(httpClient: HttpClient = applicationHttpClient) {
+    val redirects = mutableMapOf<String, String>()
+    install(Sessions) {
+        cookie<UserSession>("user_session")
+    }
+    install(Authentication) {
+        oauth("auth-oauth-google") {
+            // Configure oauth authentication
+            urlProvider = { "http://localhost:8080/callback" }
+            providerLookup = {
+                OAuthServerSettings.OAuth2ServerSettings(
+                    name = "google",
+                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+                    requestMethod = HttpMethod.Post,
+                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
+                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
+                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile"),
+                    extraAuthParameters = listOf("access_type" to "offline"),
+                    onStateCreated = { call, state ->
+                        //saves new state with redirect url value
+                        call.request.queryParameters["redirectUrl"]?.let {
+                            redirects[state] = it
+                        }
+                    }
+                )
+            }
+            client = httpClient
+        }
+    }
+    configureRouting(redirects, httpClient)
 }
