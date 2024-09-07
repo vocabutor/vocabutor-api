a3f750ba6018236bfb3174bf6fc02bb30f2dafaa
Nima Ajdari <me@nimaajdari.com>
2024-09-07 23:40:29 +0200

    feat: google oauth impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "com.vocabutor"
@@ -24,8 +25,15 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-sessions")
+    implementation("io.ktor:ktor-client-core")
+    implementation("io.ktor:ktor-client-cio")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/vocabutor/plugins/Routing.kt
+++ src/main/kotlin/com/vocabutor/plugins/Routing.kt
@@ -1,13 +1,89 @@
 package com.vocabutor.plugins
 
+import com.vocabutor.applicationHttpClient
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-fun Application.configureRouting() {
+fun Application.configureRouting(redirects: MutableMap<String, String>,
+                                 httpClient: HttpClient = applicationHttpClient) {
+
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        authenticate("auth-oauth-google") {
+            get("/login") {
+                // Redirects to 'authorizeUrl' automatically
+            }
+            get("/callback") {
+                val currentPrincipal: OAuthAccessTokenResponse.OAuth2? = call.principal()
+                // redirects home if the url is not found before authorization
+                currentPrincipal?.let { principal ->
+                    principal.state?.let { state ->
+                        call.sessions.set(UserSession(state, principal.accessToken))
+                        redirects[state]?.let { redirect ->
+                            call.respondRedirect(redirect)
+                            return@get
+                        }
+                    }
+                }
+                call.respondRedirect("/home")
+            }
+        }
+
+        get("/{path}") {
+            val userSession: UserSession? = getSession(call)
+            if (userSession != null) {
+                val userInfo: UserInfo = getPersonalGreeting(httpClient, userSession)
+                call.respondText("Hello, ${userInfo.name}!")
+            }
+        }
     }
 }
+
+private suspend fun getSession(
+    call: ApplicationCall
+): UserSession? {
+    val userSession: UserSession? = call.sessions.get()
+    //if there is no session, redirect to login
+    if (userSession == null) {
+        val redirectUrl = URLBuilder("http://0.0.0.0:8080/login").run {
+            parameters.append("redirectUrl", call.request.uri)
+            build()
+        }
+        call.respondRedirect(redirectUrl)
+        return null
+    }
+    return userSession
+}
+
+data class UserSession(val state: String, val token: String)
+
+@Serializable
+data class UserInfo(
+    val id: String,
+    val name: String,
+    @SerialName("given_name") val givenName: String,
+    @SerialName("family_name") val familyName: String,
+    val picture: String,
+//    val locale: String
+)
+
+private suspend fun getPersonalGreeting(
+    httpClient: HttpClient,
+    userSession: UserSession
+): UserInfo = httpClient.get("https://www.googleapis.com/oauth2/v2/userinfo") {
+    headers {
+        append(HttpHeaders.Authorization, "Bearer ${userSession.token}")
+    }
+}.body()
