b2dd9a37b582f251087a81fc55b2f528e1d97036
Nima Ajdari <me@nimaajdari.com>
2024-09-07 19:37:56 +0200

    initialize repo - ktor project created

---
+++ src/main/kotlin/com/vocabutor/Application.kt
@@ -0,0 +1,12 @@
+package com.vocabutor
+
+import com.vocabutor.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
