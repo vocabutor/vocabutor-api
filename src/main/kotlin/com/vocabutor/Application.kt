50a68ac45a35e5479bfcd14448c8b323ddbf03bc
Nima Ajdari <me@nimaajdari.com>
2024-09-08 17:55:01 +0200

    chore: move auth config in separate plugin file

--- src/main/kotlin/com/vocabutor/Application.kt
+++ src/main/kotlin/com/vocabutor/Application.kt
@@ -4,10 +4,7 @@ import com.vocabutor.plugins.*
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.server.application.*
-import io.ktor.server.sessions.*
 import io.ktor.client.engine.cio.*
-import io.ktor.http.*
-import io.ktor.server.auth.*
 import io.ktor.serialization.kotlinx.json.*
 
 fun main(args: Array<String>) {
@@ -22,33 +19,6 @@ val applicationHttpClient = HttpClient(CIO) {
 
 fun Application.module(httpClient: HttpClient = applicationHttpClient) {
     val redirects = mutableMapOf<String, String>()
-    install(Sessions) {
-        cookie<UserSession>("user_session")
-    }
-    install(Authentication) {
-        oauth("auth-oauth-google") {
-            // Configure oauth authentication
-            urlProvider = { "http://localhost:8080/callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "google",
-                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                    requestMethod = HttpMethod.Post,
-                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile"),
-                    extraAuthParameters = listOf("access_type" to "offline"),
-                    onStateCreated = { call, state ->
-                        //saves new state with redirect url value
-                        call.request.queryParameters["redirectUrl"]?.let {
-                            redirects[state] = it
-                        }
-                    }
-                )
-            }
-            client = httpClient
-        }
-    }
+    configureAuth(redirects, httpClient)
     configureRouting(redirects, httpClient)
 }
